1c2adc6b9451634064cb68dfcab333777525f869
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-09-24 18:12:24 +0100

    complte fourth challenge

--- src/model/products.js
+++ src/model/products.js
@@ -1,13 +1,12 @@
 const db = require("../database/db.js");
 
-/* GET SPECIFIC PRODUCT
- Write and export a new function named getProduct in model/products.
-It should take an ID, then return the matching product from the productstable. 
-It should return theidandname columns.
+/* ADD CATEGORY INFO
+Amend the query used in getProduct to also include columns called 
+category_name and category_description. You will need to retrieve this info
+from the categories table.
 
-This function is used in routes/product.js.
- Once you've finished it you should be able to see a page for one specific product if you visit the
- /product/:id in your browser (e.g. /product/1, /product/34 etc).  */
+Once complete you should see this additional info show up on the product pages in your browser
+  (e.g. /product/1). */
 
 // LIST
 const select_products = db.prepare(/*sql*/ `SELECT * FROM products`);
@@ -23,9 +22,14 @@ function searchProducts(searchQuery) {
   return search_products.all(`%${searchQuery}%`);
 }
 
-const search_single_product = db.prepare(
-  /*sql*/ `SELECT id, name FROM products WHERE id = ?`
-);
+const search_single_product =
+  db.prepare(/*sql*/ `SELECT products.id, products.name,
+  categories.name AS category_name,
+  categories.description AS category_description
+  FROM products 
+  JOIN categories
+  ON products.category_id = categories.id
+  WHERE products.id = ? `);
 function getProduct(id) {
   return search_single_product.get(id);
 }
